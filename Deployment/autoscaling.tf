11e09107d1f244e2a2690c2a70c9253967ca729e
yessinemallouli <yessine.mallouli98@gmail.com>
2022-03-13 23:04:44 +0100

    first commit

--- Deployment/autoscaling.tf
+++ Deployment/autoscaling.tf
@@ -8,41 +8,7 @@ resource "aws_launch_configuration" "levelup-launchconfig" {
   lifecycle {
     create_before_destroy = true
   }
-
-  provisioner "file" {
-      source = "elasticsearch.yml"
-      destination = "/tmp/elasticsearch.yml"
-  }
-
-  provisioner "file" {
-      source = "kibana.yml"
-      destination = "/tmp/kibana.yml"
-  }
-
-  provisioner "file" {
-      source = "apache-01.conf"
-      destination = "/tmp/apache-01.conf"
-  }
-
-    provisioner "file" {
-      source = "installELK.sh"
-      destination = "/tmp/installELK.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x    /tmp/installELK.sh",
-      "sudo sed -i -e 's/\r$//' /tmp/installELK.sh",  # Remove the spurious CR characters.
-      "sudo /tmp/installELK.sh",
-    ]
-  }
-
-  connection {
-    #host        = coalesce(self.public_ip, self.private_ip)
-    type        = "ssh"
-    user        = var.INSTANCE_USERNAME
-    private_key = file(var.PATH_TO_PRIVATE_KEY)
-  }
+  user_data = file("installELK.sh")
 }
 
 #Generate Key
